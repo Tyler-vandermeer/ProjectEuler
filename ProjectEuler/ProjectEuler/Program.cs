f849528335bdbd185cf4aa05431b20da09ac55b2
Myles <myles52@live.ca>
2017-02-28 12:47:51 -0500

    Solution 2 added.

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -11,9 +11,10 @@ namespace ProjectEuler
         static void Main(string[] args)
         {
             //Project Euler 1 : Multiples of 3 and 5
-            ProjectEulerOne one = new ProjectEulerOne();
-            one.MultiplesOf3And5();
-            //Ensure the program doesn't end bedore seeing the result.
+            ProjectEulerSolutions solution = new ProjectEulerSolutions();
+            solution.MultiplesOf3And5();
+            //Project Euler 2 : Even Fibinacci numbers
+            solution.EvenFibinacciNumbers();
             Console.ReadKey();
         }
     }
