ea00824d12d10f38d39571546dfcb2497def2cd1
Myles <myles52@live.ca>
2017-02-28 12:29:00 -0500

    Project Euler Solution 1 : Multiples of 3 and 5

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Project Euler 1 : Multiples of 3 and 5
+            ProjectEulerOne one = new ProjectEulerOne();
+            one.MultiplesOf3And5();
+            //Ensure the program doesn't end bedore seeing the result.
+            Console.ReadKey();
+        }
+    }
+}
